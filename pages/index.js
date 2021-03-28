43f9ccbbaf9ad6744b9a3dee6279d74648fbde24
Gabriel Santos <58678638+Gabriel-Almeida-Ajax@users.noreply.github.com>
2021-03-27 23:18:18 -0300

    Start

--- pages/index.js
+++ pages/index.js
@@ -2,208 +2,203 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <main>
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
+      </head>
+      <div className="container">
+        <div className="navigation">
+          <ul>
+            <li>
+              <a href="#">
+                <span className="icon"><i className="fa fa-home" aria-hidden="true" /></span>
+                <span className="tittle">
+                  Home
+              </span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="icon"><i className="fa fa-user-o" aria-hidden="true" /></span>
+                <span className="tittle">
+                  Profile
+              </span>
+              </a>
+            </li><li>
+              <a href="#">
+                <span className="icon"><i className="fa fa-commenting-o" aria-hidden="true" /></span>
+                <span className="tittle">
+                  Message
+              </span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="icon"><i className="fa fa-question-circle" aria-hidden="true" /></span>
+                <span className="tittle">
+                  Help
+              </span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="icon"><i className="fa fa-cog" aria-hidden="true" /></span>
+                <span className="tittle">
+                  Settings
+              </span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="icon"><i className="fa fa-sign-out" aria-hidden="true" /></span>
+                <span className="tittle">
+                  Sing out
+              </span>
+              </a>
+            </li>
+          </ul>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
+        <div className="toggle" />
+      </div>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              const navigation = document.querySelector('.navigation');
+  document.querySelector('.toggle').onclick = function () {
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+  }
+                  `,
+        }}
+      ></script>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+      @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap");
 
-        * {
-          box-sizing: border-box;
-        }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+body {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  min-height: 100vh;
+  background: #ff6161;
+}
+
+.container {
+  position: relative;
+}
+
+.container .navigation {
+  position: relative;
+  width: 80px;
+  height: 360px;
+  background: #fff;
+  border-radius: 10px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  -ms-border-radius: 10px;
+  -o-border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  transition: 0.5s;
+  -webkit-transition: 0.5s;
+  -moz-transition: 0.5s;
+  -ms-transition: 0.5s;
+  -o-transition: 0.5s;
+}
+
+.container .navigation.active {
+  width: 300px;
+}
+
+.container .navigation ul {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+
+.container .navigation ul li:hover {
+  background: #efefef;
+}
+
+.container .navigation ul li {
+  position: relative;
+  list-style: none;
+  width: 100%;
+}
+
+.container .navigation ul li a {
+  position: relative;
+  display: block;
+  width: 100%;
+  display: flex;
+  text-decoration: none;
+  color: #ff6161;
+  font-weight: 500;
+}
+
+.container .navigation ul li a .icon {
+  position: relative;
+  display: block;
+  min-width: 80px;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+}
+
+.container .navigation ul li a .icon .fa {
+  font-size: 24px;
+}
+
+.container .navigation ul li a .tittle {
+  position: relative;
+  display: block;
+  height: 60px;
+  line-height: 60px;
+  white-space: nowrap;
+}
+
+.toggle {
+  position: absolute;
+  right: -20px;
+  top: calc(50% - 20px);
+  width: 40px;
+  height: 40px;
+  background: #f5f5f5;
+  cursor: pointer;
+  border: 5px solid #ff6161;
+
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  -ms-border-radius: 50%;
+  -o-border-radius: 50%;
+}
+
+.toggle::before{
+  content:"\f054";
+  font-family: fontAwesome;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  line-height: 30px;
+  text-align: center;
+  color: #ff6161;
+}
+
+.toggle.active::before{
+  content:"\f053"
+}
       `}</style>
-    </div>
+    </main>
   )
 }
